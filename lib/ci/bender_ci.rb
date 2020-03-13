2685f459f6e3b52d08de94a82154a3cd346e3f45
madcato <daniel@veladan.org>
2020-03-13 13:31:12 +0100

    update bender.yml

--- lib/ci/bender_ci.rb
+++ lib/ci/bender_ci.rb
@@ -9,10 +9,10 @@ $oldrev  = ARGV[1]
 $newrev  = ARGV[2]
 $user    = ENV['USER']
 
-$project_running_dir = '%PROJECT_RUNNING_DIR%'
+$project_running_dir = '%PROJECT_RUNNING_DIR1%'
 
 puts "Launching bender yml"
-`ansible %REMOTE_MACHINE% -m shell -a "cd %PROJECT_RUNNING_DIR% && bender yml #{$refname} #{$oldrev} #{$newrev} #{$user}`
+`ansible %REMOTE_MACHINE% -m shell -a "cd %PROJECT_RUNNING_DIR2% && bender yml #{$refname} #{$oldrev} #{$newrev} #{$user}`
 TEXT
 
 class BenderCi
@@ -21,7 +21,8 @@ class BenderCi
       if current_dir_bare_git?
         open($fileName, "w") do |file|
           $updateContent["%REMOTE_MACHINE%"] = remoteMachine
-          $updateContent["%PROJECT_RUNNING_DIR%"] = installDir
+          $updateContent["%PROJECT_RUNNING_DIR1%"] = installDir
+          $updateContent["%PROJECT_RUNNING_DIR2%"] = installDir
           file << $updateContent
           puts("#{$fileName} created.")
         end
